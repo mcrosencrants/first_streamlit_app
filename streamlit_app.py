247e94decbf742f54e3cb63f097d2a63746adbd1
Marat Rosencrants <57516139+mcrosencrants@users.noreply.github.com>
2022-07-11 16:01:19 -0500

    Update streamlit_app.py
    removing unwanted indents

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ streamlit.dataframe(fruits_to_show)
 # create the function
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 
 # new section to display api
 streamlit.header("Fruityvice Fruit Advice!")
