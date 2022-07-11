73c47c6f8681b3e6d4a3982100e30f19f14c5c2e
Marat Rosencrants <57516139+mcrosencrants@users.noreply.github.com>
2022-07-11 16:06:09 -0500

    Update streamlit_app.py
    removed stop function

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,9 +47,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 # don't run anything past here while we troubleshoot
-streamlit.stop()
-
-
+# streamlit.stop()
 
 streamlit.header("The fruit load list contains")
 # Snowflake-related functions
