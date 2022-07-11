7fa751ccfc2686594658afb0c22a15553654e53c
Marat Rosencrants <57516139+mcrosencrants@users.noreply.github.com>
2022-07-11 11:22:53 -0500

    Update streamlit_app.py
    some more functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,9 @@ streamlit.dataframe(fruits_to_show)
 # new section to display api
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# removed line below
+#streamlit.text(fruityvice_response.json())
 
 # something about normalization? probably so it will show a table with appropriate
 # columns and so the text is legible
